84df6b79945bd4d7947df8510f030bf003538a1f
Omaradnax <91502202+Omaradnax@users.noreply.github.com>
2021-11-13 22:22:33 +0600

    Create 0.py

---
+++ 0.py
@@ -0,0 +1,28 @@
+import os, sys
+import readline
+from time import sleep as timeout
+from core.lzmcore import *
+
+def main():
+	banner()
+	print("   [01] Information Gathering")
+	print("   [02] Vulnerability Analysis")
+	print("   [03] Web Hacking")
+	print("   [04] Database Assessment")
+	print("   [05] Password Attacks")
+	print("   [06] Wireless Attacks")
+	print("   [07] Reverse Engineering")
+	print("   [08] Exploitation Tools")
+	print("   [09] Sniffing and Spoofing")
+	print("   [10] Reporting Tools")
+	print("   [11] Forensic Tools")
+	print("   [12] Stress Testing")
+	print("   [13] Install Linux Distro")
+	print("   [14] Termux Utility")
+	print("   [15] Shell Function [.bashrc]")
+	print("   [16] Install CLI Games")
+	print("   [17] Malware Analysis")
+	print("   [18] Compiler/Interpreter")
+	print("   [19] Social Engineering Tools")
+	print("\n   [00] Exit the Lazymux\n")
+	lazymux = input("lzmx > set_install ")
